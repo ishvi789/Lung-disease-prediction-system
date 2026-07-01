d389f64e9a005b065f5670de4018ce1e0d5f5c7e
ishvi789 <ishvi789@example.com>
2026-07-01 21:25:22 +0530

    Fix Django WSGI import path

--- django_app/django_app/wsgi.py
+++ django_app/django_app/wsgi.py
@@ -9,5 +9,5 @@ for path in [ROOT_DIR, PROJECT_DIR]:
     if str(path) not in sys.path:
         sys.path.insert(0, str(path))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.django_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
 application = get_wsgi_application()
